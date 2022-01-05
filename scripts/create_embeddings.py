d42f91652c18dafc901611b93b113c7b84e5b5aa
Sanzhar <48170101+SanzharMrz@users.noreply.github.com>
2022-01-06 02:12:12 +0600

    Update create_embeddings.py

--- scripts/create_embeddings.py
+++ scripts/create_embeddings.py
@@ -5,15 +5,10 @@ from services_embeddings import get_train_test_data
 combinations = [
 #                ([1, 1, 0, 0, 0, 0], False, False),
 #                ([1, 1, 0, 0, 0, 0], True, False),]
-                ([1, 1, 0, 0, 0, 0], False, True),
-                ([1, 1, 0, 0, 0, 0], True, True),
-                ([1, 1, 1, 0, 0, 0], False, False),
-                ([1, 1, 1, 0, 0, 0], True, False),
-                ([1, 1, 1, 0, 0, 0], False, True),
-                ([1, 1, 1, 0, 0, 0], True, True)
+                ([1, 1, 1, 1, 1, 1], False, False)
 ]
     
-os.makedirs('./vectors/', exist_ok=True)
+os.makedirs('./vectors_recomputed_sum/', exist_ok=True)
 
 for comb in combinations:
     attentions_types, use_bert, use_lmms = comb
